b3a5c255076eddde7302753317d49ae48bebccf2
saebyn <me@saebyn.dev>
2026-05-17 10:16:59 -0700

    Update .config/nvim/lua/config/options.lua

--- home/dot_config/nvim/lua/config/options.lua
+++ home/dot_config/nvim/lua/config/options.lua
@@ -8,4 +8,3 @@ vim.opt.expandtab = true -- Converts tabs to spaces
 vim.opt.shiftwidth = 2 -- Number of spaces for each step of (auto)indent
 vim.opt.tabstop = 2 -- Number of spaces that a <Tab> in the file counts for
 vim.opt.softtabstop = 2 -- Number of spaces that a <Tab> counts for while performing editing operations
-vim.g.gdscript_recommended_style = 0
