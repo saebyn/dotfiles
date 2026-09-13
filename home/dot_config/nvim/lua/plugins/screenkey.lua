3ed5ad83c5b9aeb30e6e1dbcbb214f7625d1dc13
saebyn <me@saebyn.dev>
2026-09-12 19:14:52 -0700

    feat(nvim): enable screenkey in stream mode
    Closes #10

--- home/dot_config/nvim/lua/plugins/screenkey.lua
+++ home/dot_config/nvim/lua/plugins/screenkey.lua
@@ -2,4 +2,12 @@ return {
   "NStefan002/screenkey.nvim",
   lazy = false,
   version = "*", -- or branch = "main", to use the latest commit
+  config = function(_, opts)
+    local screenkey = require("screenkey")
+    screenkey.setup(opts)
+
+    if vim.uv.fs_stat(vim.fn.expand("~/.stream-mode")) and not screenkey.is_active() then
+      screenkey.toggle()
+    end
+  end,
 }
